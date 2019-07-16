f6c9e1ba71de058b876c420a998e62b2a7aed24b
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2019-07-16 12:56:29 +0530

    Partially disable webmentions tag

---
+++ _plugins/shush_webmentions.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module WebmentionIO
+    class WebmentionTag < Liquid::Tag
+      def render(context)
+        return ''
+      end
+    end
+  end
+end
