90bd5c0124d39e996b728256c78a81373fa44bc4
Ashwin Maroli <ashmaroli@users.noreply.github.com>
2019-07-16 13:06:31 +0530

    Override webmention tag constructor

--- _plugins/shush_webmentions.rb
+++ _plugins/shush_webmentions.rb
@@ -3,6 +3,10 @@
 module Jekyll
   module WebmentionIO
     class WebmentionTag < Liquid::Tag
+      def initialize(tag_name, text, tokens)
+        super
+      end
+
       def render(context)
         return ''
       end
